3f3190db8e28945692eac4bffa596978b3268b95
GaganLal <lalgagan@outlook.com>
2019-02-13 00:00:11 +0000

    added new function to add messages, added submit form, and added submit funciton

--- src/App.jsx
+++ src/App.jsx
@@ -21,14 +21,30 @@ class App extends Component {
   }
 
   componentDidMount() {
-      
+    console.log("componentDidMount <App />");
+    setTimeout(() => {
+      console.log("Simulating incoming message");
+      // Add a new message to the list of messages in the data store
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage)
+      // Update the state of the app component.
+      // Calling setState will trigger a call to render() in App and all child components.
+      this.setState({messages: messages})
+    }, 3000);
   }
 
+  addMessage = (name, message) => {
+    const newMessage = {username: name, content: message}
+    const messages = this.state.messages.concat(newMessage)
+
+    this.setState({messages: messages})
+  }
   
   render() {
+
     return (
       <div>
-      <ChatBar userName={this.state.currentUser.name} />
+      <ChatBar userName={this.state.currentUser.name} addMessage={this.addMessage} />
       <MessageList messages={this.state.messages} />
       </div>
     );
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -2,11 +2,20 @@ import React, {Component} from 'react';
 
 function ChatBar(props) {
 
+  const onSubmit = (event) => {
+    event.preventDefault();
+    const user = event.target.elements.username.value;
+    const message = event.target.elements.messaging.value;
+    props.addMessage(user,message);
+  }
     return (
+        <form onSubmit={onSubmit}>
         <footer className="chatbar">
-        <input className="chatbar-username" defaultValue={props.userName} placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
+        <input className="chatbar-username" name ="username" defaultValue={props.userName} placeholder="Your Name (Optional)" />
+        <input className="chatbar-message" name="messaging" placeholder="Type a message and hit ENTER" />
+        <input type="submit" value="submit" />
       </footer>
+      </form>
     );
 }
 export default ChatBar;
